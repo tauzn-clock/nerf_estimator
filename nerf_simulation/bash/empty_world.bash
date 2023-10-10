5efb37922bbb6237d4dbc6d617c6b3fc13cd10db
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-10 20:39:52 +0100

    include navi demo

--- nerf_simulation/bash/empty_world.bash
+++ nerf_simulation/bash/empty_world.bash
@@ -2,4 +2,6 @@ roslaunch nerf_simulation empty_world.launch &
 sleep 5
 roslaunch jackal_gazebo spawn_jackal.launch x:=2 y:=2 &
 sleep 5
-roslaunch nerf_simulation camera_rviz.launch
+roslaunch jackal_navigation odom_navigation_demo.launch &
+sleep 5
+roslaunch jackal_viz view_robot.launch config:=navigation
