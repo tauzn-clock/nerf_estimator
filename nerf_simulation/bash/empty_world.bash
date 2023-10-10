66e59658dcd2988555be9eee5cbd0b85be1028fa
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-10 21:03:28 +0100

    bash script with navigation

--- nerf_simulation/bash/empty_world.bash
+++ nerf_simulation/bash/empty_world.bash
@@ -1,7 +1,7 @@
 roslaunch nerf_simulation empty_world.launch &
 sleep 5
-roslaunch jackal_gazebo spawn_jackal.launch x:=2 y:=2 &
+roslaunch jackal_gazebo spawn_jackal.launch x:=2 y:=2 joystick:=false config:=front_laser&
 sleep 5
 roslaunch jackal_navigation odom_navigation_demo.launch &
 sleep 5
-roslaunch jackal_viz view_robot.launch config:=navigation
+roslaunch nerf_simulation camera_rviz.launch
