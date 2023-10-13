a44a5d296334eacd496e46a532fc51ddd04e10b7
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-13 16:50:10 +0100

    more lines?

--- nerf_simulation/bash/empty_world_turtle.bash
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -2,8 +2,12 @@ export TURTLEBOT3_MODEL=burger
 export LDS_MODEL=LDS-01
 roslaunch nerf_simulation empty_world.launch &
 sleep 5
-roslaunch nerf_simulation spawn_turtlebot.launch &
+roslaunch nerf_simulation spawn_turtle.launch &
 sleep 5
-roslaunch turtlebot3_navigation move_base.launch &
+roslaunch turtlebot3_bringup turtlebot3_remote.launch model:=TURTLEBOT3_MODEL&
+sleep 5
+roslaunch turtlebot3_navigation amcl.launch &
+sleep 5
+roslaunch turtlebot3_navigation move_base.launch model:=TURTLEBOT3_MODEL&
 sleep 5
 roslaunch nerf_simulation camera_rviz.launch
