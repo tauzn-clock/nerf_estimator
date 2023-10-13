ebb7e6d22070448f09b6902e1139b9ee1dce5f4a
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-13 15:59:35 +0100

    option to spawn turtle instead

--- nerf_simulation/bash/empty_world_jackal.bash
+++ nerf_simulation/bash/empty_world_jackal.bash
@@ -1,6 +1,6 @@
 roslaunch nerf_simulation empty_world.launch &
 sleep 5
-roslaunch jackal_gazebo spawn_jackal.launch x:=2 y:=2 joystick:=false config:=front_laser&
+roslaunch jackal_gazebo spawn_jackal.launch joystick:=false config:=front_laser&
 sleep 5
 roslaunch jackal_navigation odom_navigation_demo.launch &
 sleep 5
---
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -0,0 +1,4 @@
+roslaunch nerf_simulation empty_world.launch &
+sleep 5
+roslaunch nerf_simulation spawn_turtle.launch &
+sleep 5
