408fecf1678e5bfdc0bc14f722e3675c99e1a525
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-13 19:59:33 +0100

    custom move base for turtle

--- nerf_simulation/bash/empty_world_turtle.bash
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -4,8 +4,6 @@ roslaunch nerf_simulation empty_world.launch &
 sleep 5
 roslaunch nerf_simulation spawn_turtle.launch &
 sleep 5
-roslaunch turtlebot3_bringup turtlebot3_remote.launch&
-sleep 5
-roslaunch turtlebot3_navigation move_base.launch&
+roslaunch nerf_simulation odom_navigation_demo.launch &
 sleep 5
 roslaunch nerf_simulation camera_rviz.launch
