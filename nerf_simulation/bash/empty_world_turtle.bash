c65d14bdb5304f99efc18f34c12c730e606ab708
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-13 16:39:49 +0100

    swap to simiulation

--- nerf_simulation/bash/empty_world_turtle.bash
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -2,6 +2,8 @@ export TURTLEBOT3_MODEL=burger
 export LDS_MODEL=LDS-01
 roslaunch nerf_simulation empty_world.launch &
 sleep 5
-roslaunch turtlebot3_bringup turtlebot3_robot.launch &
+roslaunch nerf_simulation spawn_turtlebot.launch &
 sleep 5
-roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/map.yaml
+roslaunch turtlebot3_navigation move_base.launch &
+sleep 5
+roslaunch nerf_simulation camera_rviz.launch
