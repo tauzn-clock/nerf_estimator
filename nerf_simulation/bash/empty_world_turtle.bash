d74ec9f7472b02dda1d7294bdee084cec479a68a
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-13 20:26:08 +0100

    so tired

--- nerf_simulation/bash/empty_world_turtle.bash
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -4,6 +4,10 @@ roslaunch nerf_simulation empty_world.launch &
 sleep 5
 roslaunch nerf_simulation spawn_turtle.launch &
 sleep 5
-roslaunch nerf_simulation odom_navigation_demo.launch &
+roslaunch turtlebot3_bringup turtlebot3_remote.launch &
+sleep 5
+roslaunch turtlebot3_navigation amcl.launch &
+sleep 5
+roslaunch nerf_simulation odom_navigation_turtle.launch &
 sleep 5
 roslaunch nerf_simulation camera_rviz.launch
