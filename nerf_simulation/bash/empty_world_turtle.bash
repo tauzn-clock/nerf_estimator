228154ac714d61e3924a9d5d7bcc2f85990816c9
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-13 16:24:06 +0100

    trying to get turtle bot to work

--- nerf_simulation/bash/empty_world_turtle.bash
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -1,4 +1,8 @@
+export TURTLEBOT3_MODEL=burger
 roslaunch nerf_simulation empty_world.launch &
 sleep 5
-roslaunch nerf_simulation spawn_turtle.launch &
+roslaunch turtlebot3_bringup turtlebot3_robot.launch &
 sleep 5
+roslaunch turtlebot3_navigation move_base.launch &
+sleep 5
+roslaunch nerf_simulation camera_rviz.launch
