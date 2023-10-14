7291acead2c47377d189442403c4b73a14ffa4a7
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-14 11:19:17 +0100

    get blank map

--- nerf_simulation/bash/empty_world_turtle.bash
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -4,5 +4,5 @@ roslaunch nerf_simulation empty_world.launch &
 sleep 5
 roslaunch nerf_simulation spawn_turtle.launch &
 sleep 5
-roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=gmapping &
+roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/map.yaml &
 sleep 5
