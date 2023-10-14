40e55dc7f6a937e9600801203a254387674b74fe
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-14 13:12:31 +0100

    change jackal name

--- nerf_simulation/bash/empty_world_jackal.bash
+++ nerf_simulation/bash/empty_world_jackal.bash
@@ -4,4 +4,4 @@ roslaunch jackal_gazebo spawn_jackal.launch joystick:=false config:=front_laser&
 sleep 5
 roslaunch jackal_navigation odom_navigation_demo.launch &
 sleep 5
-roslaunch nerf_simulation camera_rviz.launch
+roslaunch nerf_simulation rviz_jackal.launch
