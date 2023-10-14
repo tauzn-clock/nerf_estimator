c3ef2720b8d09f4be254d8b825584c0b55d21ceb
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-14 10:34:23 +0100

    update turtle test

--- nerf_simulation/bash/empty_world_turtle.bash
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -4,10 +4,5 @@ roslaunch nerf_simulation empty_world.launch &
 sleep 5
 roslaunch nerf_simulation spawn_turtle.launch &
 sleep 5
-roslaunch turtlebot3_bringup turtlebot3_remote.launch &
+roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=gmapping &
 sleep 5
-roslaunch turtlebot3_navigation amcl.launch &
-sleep 5
-roslaunch nerf_simulation odom_navigation_turtle.launch &
-sleep 5
-roslaunch nerf_simulation camera_rviz.launch
