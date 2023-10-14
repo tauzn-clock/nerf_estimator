ccce2a74ffdc29530c4e6aba91ecbf558841c252
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-14 12:26:48 +0100

    define relative path

--- nerf_simulation/bash/empty_world_turtle.bash
+++ nerf_simulation/bash/empty_world_turtle.bash
@@ -1,8 +1,11 @@
+# Get the location of the script
+map_location="$(dirname "$0")/../maps"
+echo map_location
 export TURTLEBOT3_MODEL=burger
 export LDS_MODEL=LDS-01
 roslaunch nerf_simulation empty_world.launch &
 sleep 5
 roslaunch nerf_simulation spawn_turtle.launch &
 sleep 5
-roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/map.yaml &
+roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$map_location/blank_map.yaml &
 sleep 5
