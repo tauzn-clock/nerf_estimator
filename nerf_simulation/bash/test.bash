c58da19b32497c7953bd6b728ae83e2e3590d1c4
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-14 12:48:51 +0100

    del test bash

--- nerf_simulation/bash/test.bash
+++
@@ -1,5 +0,0 @@
-# Get the full path to the directory containing the currently executing script
-script_location="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-
-# Use the script location as a variable
-echo "Script location: $script_location"
