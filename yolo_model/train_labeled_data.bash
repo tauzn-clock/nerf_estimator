bdce1409f68e0eb2debe6d82b5bb80dfc4d81a46
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2023-10-31 20:10:29 +0000

    first yolo

---
+++ yolo_model/train_labeled_data.bash
@@ -0,0 +1,10 @@
+#Get the directory the bash file is currently in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
+
+yolo task=detect \
+mode=train \
+model=yolov8n.pt \
+data= $DIR/jackal_detection-2/data.yaml \
+epochs=100 \
+imgsz=640
