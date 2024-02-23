f781e77943a07c1babcd6574b6966c41bb10284a
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-02-23 12:37:52 +0000

    clean up code for floor

--- blender/scripts/get_scene_static_camera.py
+++ blender/scripts/get_scene_static_camera.py
@@ -4,10 +4,22 @@ import json
 import numpy as np
 from scipy.spatial.transform import Rotation
 
-def set_camera(camera_name, xyz, rpy):
+def set_object_pose(name, xyz, rpy):
     #Set camera position
-    bpy.data.objects[camera_name].location = xyz
-    bpy.data.objects[camera_name].rotation_euler = rpy
+    bpy.data.objects[name].location = xyz
+    bpy.data.objects[name].rotation_euler = rpy
+
+def get_pose(object):
+    pose = np.eye(4)
+    pose[:3,3] = np.array([object.location.x,object.location.y,object.location.z])
+    pose[:3,:3] = Rotation.from_euler("xyz",(object.rotation_euler.x,object.rotation_euler.y,object.rotation_euler.z),degrees=True).as_matrix()
+    return pose
+def invert_pose(pose):
+    inverse = np.eye(4)
+    inverse[:3,:3] = pose[:3,:3].T
+    inverse[:3,3] = - pose[:3,:3].T @ pose[:3,3]
+    return inverse
+
 
 # Set light
 light_name = 'Light'
@@ -20,7 +32,7 @@ FILE_PATH = os.path.realpath(__file__)
 FILE_PATH = FILE_PATH.split('/')
 FILE_PATH = '/'.join(FILE_PATH[:-3])
 
-DATA_PATH = os.path.join(FILE_PATH, 'data/jackal_training_data_1')
+DATA_PATH = os.path.join(FILE_PATH, 'data/jackal_floor_training_data_1')
 print(DATA_PATH)
 
 # Get transform
@@ -45,29 +57,42 @@ with open(OUTPUT_FILE,'w') as f:
     f.write("Angle:" + str(bpy.data.objects[camera_name].data.angle) + "\n")
     f.write("Angle X:" + str(bpy.data.objects[camera_name].data.angle_x) + "\n")
     f.write("Angle Y:" + str(bpy.data.objects[camera_name].data.angle_y) + "\n")
-
+    f.write(str(bpy.data.collections.get("visual")))
+    
+cnt = 0
 for frame in transforms['frames']:
     file_path = os.path.join(DATA_PATH, frame['file_path'])
     transform_matrix = np.asarray(frame['transform_matrix'])
-
-    xyz = transform_matrix[:3, 3]
-    rpy = transform_matrix[:3, :3]
-    rpy = Rotation.from_matrix(rpy).as_euler('xyz', degrees=False)
+    camera_xyz = transform_matrix[:3, 3]
+    camera_rpy = transform_matrix[:3, :3]
+    camera_rpy = Rotation.from_matrix(camera_rpy).as_euler('xyz', degrees=False)
 
     #Transform back to origin
-    inverse = np.ones(4)
-    inverse[:3,:3] = transform_matrix[:3,:3].T
-    inverse[:3,3] = -np.dot(transform_matrix[:3,:3].T, transform_matrix[:3,3])
-
-    #Translate to x = -2
-    translate = np.ones(4)
-    translate[:3,0] = -2
-
-
-    set_camera(camera_name, xyz, rpy)
-
+    inverse = invert_pose(transform_matrix)
+    
+    #Translate to y = -2, align and pitch down
+    final_pose = np.eye(4)
+    final_pose[:3,:3] = Rotation.from_euler('xyz',(camera_rpy[0],0,0),degrees=False).as_matrix()
+    final_pose[:3,3] = np.array([0,-2,camera_xyz[2]])
+    
+    camera_transform = final_pose
+    camera_xyz = camera_transform[:3, 3]
+    camera_rpy = camera_transform[:3, :3]
+    camera_rpy = Rotation.from_matrix(camera_rpy).as_euler('xyz', degrees=False)
+    set_object_pose(camera_name, camera_xyz, camera_rpy)
+    
+    object_transform = final_pose @ invert_pose(transform_matrix)
+    object_xyz = object_transform[:3, 3]
+    object_rpy = object_transform[:3, :3]
+    object_rpy = Rotation.from_matrix(object_rpy).as_euler('xyz', degrees=False)
+    
+    set_object_pose("base_link",object_xyz,object_rpy)
+    
     # Render Path
     bpy.context.scene.render.filepath = os.path.join(DATA_PATH,file_path)
 
     # Render scene
     bpy.ops.render.render(write_still=True)
+    
+    cnt += 1
+    if (cnt==3): break
